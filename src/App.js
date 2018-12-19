64d6b398beba5577932e8d8befaee581f6d66cc2
dmarnol2 <dmarnol2@asu.edu>
2018-12-18 23:27:08 -0500

    init React skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
+import { Container, Button, 
+        Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText
+      } from 'reactstrap';
+import {Grid, Row, Col} from 'react-flexbox-grid';
 import logo from './logo.svg';
 import './App.css';
+// import './bootstrap-social.css';
+import {
+  Collapse, Navbar, NavbarToggler,NavbarBrand,Nav,NavItem,
+  NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+var {SocialIcon} = require('react-social-icons');
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      'email':'',
+      'password':'',
+      validate: {emailState: ''},
+  }
+  this.handleChange = this.handleChange.bind(this);
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h3>Portfolio template in React</h3>
+            
+
+          
         </header>
       </div>
     );
